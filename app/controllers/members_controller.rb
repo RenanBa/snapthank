41bafd411d538d55e62e912f2a43c403db604e8e
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-11 15:35:17 -0700

    Add skip_before_filter to create memeber

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,53 @@
 class MembersController < ApplicationController
+  def index
+    @members = Member.all
+    render json: @members
+  end
+
+  def show
+    @member = Member.find(params[:id])
+    render json: @member
+  end
+
+  def new
+    @member = Member.new
+    render json: "NEW"
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def create
+    render json: "CREATE"
+    # @member = Member.new(member_params)
+
+    # if @member.save
+    #   redirect_to @member
+    # else
+    #   render 'new'
+    # end
+  end
+
+  def update
+    @member = Member.find(params[:id])
+
+    if @member.update(member_params)
+      redirect_to @member
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+
+    redirect_to members_path
+  end
+
+  private
+    def member_params
+      params.require(:member).permit(:email, :name)
+    end
 end
