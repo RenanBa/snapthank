02f041228118c7398ec1a946e2a301b6860a743f
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-14 16:16:33 -0700

    Add gmail delivery method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
    default from: 'renancontactme@gmail.com'
 
-  def welcome_email(user)
-    @user = user
+  def welcome_email(member)
+    @member = member
     @url  = 'http://localhost:3000/members/'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @member.email, subject: 'Welcome to My Awesome Site')
   end
 end
