de6f41612af9096445b3b58d4fd12259ce6c80bc
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-15 23:38:12 -0700

    Video record with audio but need to mute when video is recorded

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,7 @@
 console.log("main.js");
 // HD constraints
 var constraints = {
+  audio: true,
   video: {
     mandatory: {
       minWidth: 1280,
@@ -12,23 +13,6 @@ var constraints = {
 var errorCallback = function(e) {
   console.log('Reeeejected!', e);
 };
-
-
-
-
-
-  // // Not showing vendor prefixes.
-  // navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
-  //   var video = document.querySelector('video');
-  //   video.src = window.URL.createObjectURL(localMediaStream);
-
-  //   // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-  //   // See crbug.com/110938.
-  //   video.onloadedmetadata = function(e) {
-  //     // Ready to go. Do some stuff.
-  //   };
-  // }, errorCallback);
-
 
 var mediaRecorder;
 var chunks = [];
