ad743559648f0e0c281f2631af6adf473dd35038
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-17 19:00:45 -0700

    Add ip checker before create a new donor

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -40,17 +40,21 @@ class DonorsController < ApplicationController
 
   # Create with mailer action call
   def create
-    @donor = Donor.new(email: params[:email], name: params[:name], donation: params[:donation])
-    respond_to do |format|
-      if @donor.save
-        @member = select_member(@donor)
-        UserMailer.welcome_email(@member, @donor).deliver_later
-        format.html { redirect_to(@donor, notice: 'donor was successfully created.') }
-        format.json { render json: @donor, status: :created, location: @donor }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @donor.errors, status: :unprocessable_entity }
+    if request_ip(request.ip)
+      @donor = Donor.new(email: params[:email], name: params[:name], donation: params[:donation])
+      respond_to do |format|
+        if @donor.save
+          @member = select_member(@donor)
+          UserMailer.welcome_email(@member, @donor).deliver_later
+          format.html { redirect_to(@donor, notice: 'donor was successfully created.') }
+          format.json { render json: @donor, status: :created, location: @donor }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @donor.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      render json: "Access not authorized"
     end
   end
 
