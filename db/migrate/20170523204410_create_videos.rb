58b0c906435aecc01f0b1821ec2f1850f8f4994b
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-23 13:45:11 -0700

    Create new video table with new rolls

---
+++ db/migrate/20170523204410_create_videos.rb
@@ -0,0 +1,16 @@
+class CreateVideos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :videos do |t|
+      t.string :link
+      t.string :title
+      t.datetime :published_at
+      t.integer :likes
+      t.integer :dislikes
+      t.string :uid
+      t.references :donor, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :videos, :uid
+  end
+end
