9f347fb0ee421934c823bd7820111114aa9e3fc2
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-23 14:29:49 -0700

    Add method before_save into models videos because of error with gem that riquered to create a new model called VideoObserver but doest works

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,15 @@ class Video < ApplicationRecord
 
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
   validates :link, presence: true, format: YT_LINK_FORMAT
+
+  def before_save(resource)
+    video = Yt::Video.new url: resource.link
+    resource.uid = video.id
+    resource.title = video.title
+    resource.likes = video.like_count
+    resource.dislikes = video.dislike_count
+    resource.published_at = video.published_at
+  rescue Yt::Errors::NoItems
+    resource.title = ''
+  end
 end
