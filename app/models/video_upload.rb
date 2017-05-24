fda5cdcc9c34a6ca21ddaf96c5bacb54ec032584
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-24 11:57:41 -0700

    Create method to upload video in the video_upload model

--- app/models/video_upload.rb
+++ app/models/video_upload.rb
@@ -5,4 +5,9 @@ class VideoUpload < ActiveType::Object
 
   validates :file, presence: true
   validates :title, presence: true
+
+  def upload!(user)
+    account = Yt::Account.new access_token: user.token
+    account.upload_video self.file, title: self.title, description: self.description
+  end
 end
