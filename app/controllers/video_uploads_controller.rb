a60572167b51e814650285ede909cfcd071eb08d
RenanBa <renanbarbosadesouza@gmail.com>
2017-05-24 11:56:27 -0700

    Implement create method in VideoUploadsController

--- app/controllers/video_uploads_controller.rb
+++ app/controllers/video_uploads_controller.rb
@@ -4,5 +4,17 @@ class VideoUploadsController < ApplicationController
   end
 
   def create
+    @video_upload = VideoUpload.new(title: params[:video_upload][:title],
+                                  description: params[:video_upload][:description],
+                                  file: params[:video_upload][:file].try(:tempfile).try(:to_path))
+    if @video_upload.save
+      uploaded_video = @video_upload.upload!(current_user)
+
+      # check if the video was uploaded or not
+
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 end
