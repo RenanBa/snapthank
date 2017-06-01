fe3328ca909ba81540c97513668d02fdc4aada68
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-01 13:53:47 -0700

    Add validation to donors, move the mailer action call into if dono was save

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -41,16 +41,16 @@ class DonorsController < ApplicationController
     if request_ip(request.ip)
       @donor = Donor.new(email: params[:email], name: params[:name],
                          donation: params[:donation], key: SecureRandom.hex(32))
-      respond_to do |format|
-        if @donor.save
+      if @donor.save
+        respond_to do |format|
           @member = select_member(@donor)
           UserMailer.welcome_email(@member, @donor).deliver_later
-          format.html { redirect_to(@donor, notice: 'donor was successfully created.') }
+          # format.html { redirect_to(@donor, notice: 'donor was successfully created.') }
           format.json { render json: @donor, status: :created, location: @donor }
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @donor.errors, status: :unprocessable_entity }
         end
+      else
+        # format.html { render action: 'show' }
+        format.json { render json: @donor.errors, status: :unprocessable_entity }
       end
     else
       render json: "Access not authorized"
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,3 +1,4 @@
 class Donor < ApplicationRecord
   has_many :videos, dependent: :destroy
+  validates :name, :email, :donation, presence: true
 end
