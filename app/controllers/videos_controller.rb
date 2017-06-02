1ca9dece06dd12a2f9ab6f2ae68097e0c92ade35
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-01 18:57:12 -0700

    Clean the the format email code from videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,31 +14,26 @@ class VideosController < ApplicationController
     @video_upload = @donor.videos.create(title: params[:title],
                                     description: params[:description],
                                     file: params[:webmasterfile].try(:tempfile).try(:to_path))
-    # respond_to do |format|
-      if @video_upload.save
-        5.times{p "SAVED"}
-        uploaded_video = @video_upload.upload!(current_user)
-        if uploaded_video.failed?
-          flash[:error] = 'There was an error while uploading your video...'
-          format.html { redirect_to(@donor, notice: "video wasn't uploaded.") }
-          format.json { render json: @video_upload.errors, status: :unprocessable_entity }
-        else
-          5.times{p "creating link"}
-          @video_upload.update!(link: uploaded_video.id)
-          respond_to do |format|
-            UserMailer.thanks_email(@donor, @video_upload).deliver_later
-            format.html { redirect_to(root_url, notice: 'video link was successfully created and uploaded.') }
-            format.json { render json: @video_upload, status: :created, location: @video_upload }
-          end
-        end
-        session[:donor_key] = nil
-        session[:id_donor] = nil
-        @donor.destroy
+    if @video_upload.save
+      5.times{p "SAVED"}
+      uploaded_video = @video_upload.upload!(current_user)
+      if uploaded_video.failed?
+        flash[:error] = 'There was an error while uploading your video...'
       else
-        format.html { redirect_to(@donor, notice: "video wasn't uploaded.") }
-        format.json { render json: @video_upload.errors, status: :unprocessable_entity }
+        5.times{p "creating link"}
+        @video_upload.update!(link: uploaded_video.id)
+        respond_to do |format|
+          UserMailer.thanks_email(@donor, @video_upload).deliver_later
+          format.html { redirect_to(root_url, notice: 'video link was successfully created and uploaded.') }
+          format.json { render json: @video_upload, status: :created, location: @video_upload }
+        end
       end
-    # end
+      session[:donor_key] = nil
+      session[:id_donor] = nil
+      @donor.destroy
+    else
+      redirect_to(@donor, notice: "video wasn't uploaded.")
+    end
   end
   # private
   #   def video_params
