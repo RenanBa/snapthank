897f8d9227cc6e3adaaa31e24c20113e7159cc82
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-04 17:56:46 -0700

    Add self on the destroy donor method and hide lanscape image for desktop

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,7 +2,7 @@ class Donor < ApplicationRecord
   has_many :videos, dependent: :destroy
   validates :name, :email, :donation, presence: true
 
-  def destroy_donor(donor)
+  def self.destroy_donor(donor)
     5.times{p "Destroying donor"}
     session[:donor_key] = nil
     session[:id_donor] = nil
