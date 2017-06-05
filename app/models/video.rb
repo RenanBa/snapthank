07d12d4e769c425eb6f501a7195cd4d7d2b28374
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-05 12:20:16 -0700

    Testing error with wrong user token

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ApplicationRecord
   belongs_to :donor
   validates :file, presence: true
   def upload!(user)
-    5.times{ p "Model video_upload"}
+    5.times{ p "Model Upload"}
     account = Yt::Account.new access_token: user.token
     account.upload_video self.file, title: self.title, description: self.description, privacy_status: "unlisted"
   end
