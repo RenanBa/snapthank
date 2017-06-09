d5ad9f0754d7972bd6df29e1ed3efeb77791307f
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-09 12:51:42 -0700

    Add sleep mmethod into destroy donor

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -5,7 +5,9 @@ class Donor < ApplicationRecord
   def self.destroy_donor(donor, video)
     5.times{p "Destroy Donor"}
     5.times{p "Video"}
-    yield
-    10.times{p "Back to destroy_donor!!!"}
+    10.times{p Time.new }
+    sleep 4
+    10.times{p Time.new }
+    5.times{p "Back to destroy_donor!!!"}
   end
 end
