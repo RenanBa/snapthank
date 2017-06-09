914aba992a6577b1d074b2601fb58850d59262ac
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-09 12:33:27 -0700

    Found out that the object isn't being send

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -3,8 +3,8 @@ class Donor < ApplicationRecord
   validates :first_name, :email, :donation, presence: true
 
   def self.destroy_donor(donor, video)
-    5.times{p "Donor" + donor}
-    5.times{p "Video" + video}
+    5.times{p "Destroy Donor"}
+    5.times{p "Video"}
     yield
     10.times{p "Back to destroy_donor!!!"}
   end
