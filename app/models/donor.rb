8c52ad89c676734fd6163661f80da5fd0ce31a46
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-09 13:13:02 -0700

    Email sent and donor delited successfuly

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,11 +2,7 @@ class Donor < ApplicationRecord
   has_many :videos, dependent: :destroy
   validates :first_name, :email, :donation, presence: true
 
-  def self.destroy_donor(donor, video)
-
-    5.times{p "Donor #{donor}" }
-    5.times{p "Video #{video}"}
+  def self.destroy_donor(donor)
     Donor.find(donor).destroy
-    5.times{p "destroyed"}
   end
 end
