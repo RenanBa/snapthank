0e207779188e5112cdeaf6d4e53e30b5664de3a6
RenanBa <renanbarbosadesouza@gmail.com>
2017-06-09 13:09:02 -0700

    Destroy donor in the destroy donor function

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -3,7 +3,10 @@ class Donor < ApplicationRecord
   validates :first_name, :email, :donation, presence: true
 
   def self.destroy_donor(donor, video)
+
     5.times{p "Donor #{donor}" }
     5.times{p "Video #{video}"}
+    Donor.find(donor).destroy
+    5.times{p "destroyed"}
   end
 end
