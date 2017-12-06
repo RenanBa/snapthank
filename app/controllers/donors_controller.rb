b97c230d2a9ceb99157e7569b11fa877b2dfaa53
RenanBa <renanbarbosadesouza@gmail.com>
2017-12-06 14:50:15 -0800

    Add block to print the time that the email will be delivered to the member

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -54,6 +54,9 @@ class DonorsController < ApplicationController
       if @donor.save
         respond_to do |format|
           @member = select_member(@donor)
+          time = Time.new #test to see the time for delivery
+          time = time + schedule*60
+          p "New donation, schedule to " + time.to_s
           UserMailer.welcome_email(@member, @donor).deliver_later(wait: schedule.minutes)
           format.json { render json: @donor, status: :created, location: @donor }
         end
