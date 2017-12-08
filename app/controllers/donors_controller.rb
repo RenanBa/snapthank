e35e334615cdd453203d09635f4adb36bdc7da6d
RenanBa <renanbarbosadesouza@gmail.com>
2017-12-07 21:27:52 -0800

    Remove schedule and add 5 minutes to deliver email to donors

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -54,10 +54,7 @@ class DonorsController < ApplicationController
       if @donor.save
         respond_to do |format|
           @member = select_member(@donor)
-          time = Time.new #test to see the time for delivery
-          time = time + schedule*60
-          p "New donation, schedule to " + time.to_s
-          UserMailer.welcome_email(@member, @donor).deliver_later(wait: schedule.minutes)
+          UserMailer.welcome_email(@member, @donor).deliver_later
           format.json { render json: @donor, status: :created, location: @donor }
         end
       else
